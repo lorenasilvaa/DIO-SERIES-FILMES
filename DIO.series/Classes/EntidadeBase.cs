c756c753bf4e7fd7b228b235ab4c701aceca4059
lorenasilvaa <86986228+lorenasilvaa@users.noreply.github.com>
2022-04-19 11:19:15 -0300

    Add files via upload

---
+++ DIO.series/Classes/EntidadeBase.cs
@@ -0,0 +1,14 @@
+namespace DIO.Series
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+
+        public Genero Genero { get; set; }
+		public string Titulo { get; set; }
+		public string Descricao { get; set; }
+		public int Ano { get; set; }
+        public bool Excluido {get; set;}
+
+    }
+}
---
+++ DIO.series/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using DIO.series;
+
+namespace DIO.Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+			string opcaoMenuUsuario = ObterOpcaoMenuUsuario();
+			while (opcaoMenuUsuario.ToUpper() != "X")
+			{
+				switch (opcaoMenuUsuario)
+				{
+					case "1":
+						MenuFilme.Filme();
+						break;
+					case "2":
+						MenuSerie.Serie();
+						break;
+					default:
+						throw new ArgumentOutOfRangeException();
+				}
+
+				opcaoMenuUsuario = ObterOpcaoMenuUsuario();
+			}
+
+			Console.WriteLine("Obrigado por utilizar nossos serviços.");
+			Console.ReadLine();
+            
+			
+		}
+
+		public static string ObterOpcaoMenuUsuario(){
+
+			Console.WriteLine();
+			Console.WriteLine("Olá, seja bem vido(a)!");
+
+			Console.WriteLine("Você deseja: ");
+
+			Console.WriteLine("1- Acessar o menu de Filmes");
+			Console.WriteLine("2- Acessar o menu de Séries");
+			Console.WriteLine("X- Sair");
+			Console.WriteLine();
+
+			string opcaoMenuUsuario = Console.ReadLine().ToUpper();
+			Console.WriteLine();
+
+			return opcaoMenuUsuario;
+
+		}
+    }
+}
